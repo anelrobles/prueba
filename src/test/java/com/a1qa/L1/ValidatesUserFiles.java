45a13aea7b6215532a8df5615aa70bb2716d65ce
anelrobles <anelrobles@hotmail.com>
2023-02-03 08:10:35 -0700

    initial commit

---
+++ src/test/java/com/a1qa/L1/ValidatesUserFiles.java
@@ -0,0 +1,45 @@
+package com.a1qa.L1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.DataProvider;
+
+
+public class ValidatesUserFiles {
+	
+  @BeforeClass
+  public void beforeClass() {
+	  
+  }
+	 
+  @DataProvider 
+	  public Object[][] jsonAdmin(){
+	  JSONParser parser = new JSONParser();
+	  JSONObject jsonObject = null;
+	  
+	  try {
+		  Object obj = parser.parse(new FileReader(fileName "/a1qa/Files/Json/admin_users.json"));
+		  jsonObject = (JSONObject) obj;
+	  } catch (IDException | ParseException exception) {
+		  
+	  }
+	  
+		  
+  }
+	  
+	  
+  @Test
+  public void f() {
+  }
+  
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+ 
+
+}
